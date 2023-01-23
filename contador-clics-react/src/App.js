ee70dad7312f8b8cb54a3e6cf76ed983604278db
Mennuti Jeronimo <jeromennuti@gmail.com>
2023-01-23 20:36:00 -0300

    agrego contador de clics

--- contador-clics-react/src/App.js
+++ contador-clics-react/src/App.js
@@ -1,17 +1,48 @@
 import './App.css';
+import Boton from './componentes/Boton';
+import Contador from './componentes/Contador';
 import freeCodeCampLogo from './imagenes/FreeCodeCampLogo.png';
+import { useState } from 'react';
+
+
+
 
 function App() {
+
+const [numClics, setNumClics] = useState();
+
+
+
+
+  const manejarClic = () => {
+    console.log('Clic');
+  }
+
+  const reiniciarContador = () => {
+    console.log('Reiniciar');
+  }
+
+
+
   return (
     <div className="App">
-    <div className='freecodecamp-logo-contenedor'>
-      <img 
-      className='freecodecamp-logo'
-      src={freeCodeCampLogo}
-      alt='logo de freeCodeCamp'/>
-      </div>  
+      <div className='freecodecamp-logo-contenedor'>
+        <img
+          className='freecodecamp-logo'
+          src={freeCodeCampLogo}
+          alt='logo de freeCodeCamp' />
+      </div>
       <div className='contenedor-principal'>
+        <Contador numClics='5' />
+        <Boton
+          texto='Clic'
+          esBotonDeClic={true}
+          manejarClic={manejarClic} />
 
+        <Boton
+          texto='Reiniciar'
+          esBotonDeClic={false}
+          manejarClic={reiniciarContador} />
       </div>
     </div>
   );
